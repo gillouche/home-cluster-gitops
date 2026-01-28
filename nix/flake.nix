2d851951cd997750d1653f3dd5756d1fa7ddb96f
Gilles Habran <gilleshabran@protonmail.com>
2026-01-28 22:46:50 +0100

    add nix

---
+++ nix/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Home Cluster GitOps Monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = import ./shells/main.nix { inherit pkgs; };
+      }
+    );
+}
